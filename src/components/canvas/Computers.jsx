699d4d4e06182b1dd39bc61b86bab9d9c250ca0c
Salomão Nunes <reinaldo.vombo@gmail.com>
2023-04-20 14:45:48 +0000

    about

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,16 +1,16 @@
-import React, { Suspense, useEffect, useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
-import CanvasLoader from '../Loader'
+import React, { Suspense, useEffect, useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Computers = props => {
-  const mesh = useRef()
+const Computers = ({ props, isMobile }) => {
+  const mesh = useRef();
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  const computer = useGLTF('../desktop_pc/scene.gltf')
+  const computer = useGLTF('../desktop_pc/scene.gltf');
   return (
     <mesh {...props} ref={mesh}>
-      <hemisphereLight intensity={0.15} groundColor="black">
-        <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor='black'>
+        <hemisphereLight intensity={0.15} groundColor='black' />
         <pointLight intensity={1} />
         <spotLight
           position={[-20, 50, 10]}
@@ -22,19 +22,41 @@ const Computers = props => {
         />
         <primitive
           object={computer.scene}
-          scale={0.75}
-          position={[0, -4.9, -1.5]}
+          scale={isMobile ? 0.7 : 0.75}
+          position={isMobile ? [0, -3, -2.2] : [0, -4.9, -1.5]}
           rotation={[-0.01, -0.2, -0.1]}
         />
       </hemisphereLight>
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia('max-width: 500');
+
+    // Add a listener for changes to the screen size
+    setIsMobile(mediaQuery.matches);
+
+    // Add a listener for changes to the screen size
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matchMedia);
+    };
+
+    // Add a listener for changes to the screen size
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
-      frameloop="demand"
+      frameloop='demand'
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
@@ -45,11 +67,11 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
-export default ComputersCanvas
+export default ComputersCanvas;
